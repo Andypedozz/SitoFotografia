f53047514f3b0965b43a03f461136afe20833c61
Andypedozz <andypedini02@gmail.com>
2026-01-21 15:58:56 +0100

    .

--- src/pages/api/media.js
+++ src/pages/api/media.js
@@ -1,9 +1,33 @@
-
+import { Media } from "../../lib/db.js";
 /**
  * Handle GET requests for media.
  */
 export async function GET({ request }) {
+    const url = new URL(request.url);
+    const id = url.searchParams.get("id");
+    const nome = url.searchParams.get("nome");
 
+    let result;
+
+    try {
+        if (id) {
+            result = await Media.findByPk(id);
+        } else if (nome) {
+            result = await Media.findOne({ where: { nome } });
+        } else {
+            result = await Media.findAll();
+        }
+
+        // TODO: Implement GET logic
+        return new Response(JSON.stringify(result), {
+            headers: { 'Content-Type': 'application/json' }
+        });
+    } catch (err) {
+        console.error(err.message);
+        return new Response(JSON.stringify(err.message), {
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
 }
 
 /**
