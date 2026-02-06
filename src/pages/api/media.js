993a312b189d333f6ae7a64fabc5d0515022c812
Andypedozz <andypedini02@gmail.com>
2026-02-06 12:10:03 +0100

    .

--- src/pages/api/media.js
+++ src/pages/api/media.js
@@ -1,46 +1,41 @@
-import { Media } from "../../db/models.js";
-/**
- * Handle GET requests for media.
- */
+
+
 export async function GET({ request }) {
-    const url = new URL(request.url);
-    const id = url.searchParams.get("id");
-    const nome = url.searchParams.get("nome");
-    const projectId = url.searchParams.get("projectId");
 
-    let result;
 
-    try {
-        if (id) {
-            result = await Media.findOne("Media", { where : { id } });
-        } else if (nome) {
-            result = await Media.findOne("Media", { where : { nome } });
-        } else if (projectId) {
-            result = await Media.findAll("Media", { where : { idProgetto: projectId } });
-        } else {
-            result = await Media.findAll("Media");
-        }
-        return new Response(JSON.stringify(result), {
-            headers: { 'Content-Type': 'application/json' }
-        });
-    } catch (err) {
-        console.error(err.message);
-        return new Response(JSON.stringify(err.message), {
-            headers: { 'Content-Type': 'application/json' }
-        });
-    }
+
+
+    return new Response(JSON.stringify(result), {
+        headers: { 'Content-Type': 'application/json' }
+    })
 }
 
-/**
- * Handle POST requests to upload new media.
- */
 export async function POST({ request }) {
 
+
+
+
+    return new Response(JSON.stringify(result), {
+        headers: { 'Content-Type': 'application/json' }
+    })
 }
 
-/**
- * Handle DELETE requests to remove media.
- */
+export async function PUT({ request }) {
+
+
+
+
+    return new Response(JSON.stringify(result), {
+        headers: { 'Content-Type': 'application/json' }
+    })
+}
+
 export async function DELETE({ request }) {
 
+
+
+
+    return new Response(JSON.stringify(result), {
+        headers: { 'Content-Type': 'application/json' }
+    })
 }
--- src/pages/api/projects/homepage.js
+++ src/pages/api/projects/homepage.js
@@ -1,13 +1,41 @@
 
-import { Progetto } from "../../../db/models.js";
 
 export async function GET({ request }) {
-    const url = new URL(request.url);
 
-    const result = await Progetto.findAll({ where: { homePage: 1 } });
 
-    // TODO: Implement GET logic
+
+
     return new Response(JSON.stringify(result), {
         headers: { 'Content-Type': 'application/json' }
-    });
+    })
+}
+
+export async function POST({ request }) {
+
+
+
+
+    return new Response(JSON.stringify(result), {
+        headers: { 'Content-Type': 'application/json' }
+    })
+}
+
+export async function PUT({ request }) {
+
+
+
+
+    return new Response(JSON.stringify(result), {
+        headers: { 'Content-Type': 'application/json' }
+    })
+}
+
+export async function DELETE({ request }) {
+
+
+
+
+    return new Response(JSON.stringify(result), {
+        headers: { 'Content-Type': 'application/json' }
+    })
 }
