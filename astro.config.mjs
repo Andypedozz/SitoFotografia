fcab40734b9e05a3dff42afc45f951b6788b5ae3
andrea <andypedini02@gmail.com>
2026-02-16 16:04:39 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 import "./src/db/db.js"
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  })
+  adapter: vercel(),
 });
